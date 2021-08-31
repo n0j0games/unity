0e508ed461ac0b9885cd26902132a669ae4e9d86
Noah Schütte <66693138+n0j0games@users.noreply.github.com>
2021-09-01 00:47:05 +0200

    Add files via upload

---
+++ CameraSystem.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+
+//Requires Pixel-Perfect camera to be installed & added to the "Main Camera" GameObject (or some other camrea)
+//The Camera can be zoomed with the mouse wheel and moved by dragging the middle mouse button
+
+public class CameraSystem : MonoBehaviour
+{
+    /// <summary>The interval in which the camera will zoom in/out</summary>
+    public int zoominterval = 2;
+    /// <summary>Movement speed on camera drag</summary>
+    public float movespeed = 2f;
+    PixelPerfectCamera ppc;
+    float zoom;
+    Vector3 dragOrigin;
+
+    void Start()
+    {
+        ppc = gameObject.GetComponent<PixelPerfectCamera>();
+        zoom = ppc.assetsPPU;
+    }
+
+    void Update()
+    {
+        startZoom();
+        movement();
+    }
+
+    void startZoom()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+            ppc.assetsPPU += zoominterval;
+        if (Input.mouseScrollDelta.y < 0 && ppc.assetsPPU > zoominterval)
+            ppc.assetsPPU -= zoominterval;
+    }
+
+    void movement()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+        if (!Input.GetMouseButton(2)) return;
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * -movespeed, pos.y * -movespeed, 0);
+        transform.Translate(move, Space.World);
+    }
+}
